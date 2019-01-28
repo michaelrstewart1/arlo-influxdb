8b55b70e9547f72199079c121249925a80ff4658
Alexander Acuña <38297159+alexanderjacuna@users.noreply.github.com>
2019-01-27 23:28:24 -0600

    Completed script for basic monitoring.

--- arlo-influxdb.py
+++ arlo-influxdb.py
@@ -2,3 +2,42 @@
 
 from pyarlo import PyArlo
 from influxdb import InfluxDBClient
+
+# INFLUXDB CONNECTION INFO
+host = "192.168.1.67"
+port = 8086
+user = "user"
+password = "password" 
+dbname = "readings"
+
+#CREATE CLIENT OBJECT
+client = InfluxDBClient(host, port, user, password, dbname)
+
+# ARLO CONNECTION INFO
+arlo  = PyArlo('user@fake.com', 'password')
+
+# SET VARIABLES
+cam = arlo.cameras
+cam = len(cam)
+
+if cam <> 0:
+	for i in range(cam):
+		model_id = arlo.cameras[i].model_id
+		serial_number = arlo.cameras[i].serial_number
+		battery_level = arlo.cameras[i].battery_level
+		signal_strength = arlo.cameras[i].signal_strength
+		measurement = "jupitor-" + model_id + "-" + serial_number
+		
+		data = [
+		{
+		  "measurement": measurement,
+			  "fields": {
+				  "battery_level" : battery_level ,
+				  "signal_strength" : signal_strength
+			  }
+		  } 
+		]
+	
+		client.write_points(data)
+else:
+	sys.exit()
